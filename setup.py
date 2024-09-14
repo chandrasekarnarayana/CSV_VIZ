55ebec1904e99374e18e98e18976eda99c623bdd
Chandrasekar SUBRAMANI NARAYANA <chandrasekarnarayana@gmail.com>
2024-09-14 10:55:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,17 @@ setup(
     description='A CSV visualization tool with Seaborn plots and GUI using PyQt5',
     author='Chandrasekar SUBRAMANI NARAYANA',
     author_email='chandrasekarnarayana@gmail.com',
-    url='https://github.com/chandrasekarnarayana/csv_viz',
+    url='https://github.com/chandrasekarnarayana/CSV_VIZ',
     packages=find_packages(),
     install_requires=[
-        'pandas',
-        'matplotlib',
-        'seaborn',
-        'PyQt5',
+        'pandas>=1.3.0',
+        'matplotlib>=3.4.0',
+        'seaborn>=0.11.2',
+        'PyQt5>=5.12',
     ],
     entry_points={
         'console_scripts': [
-            'csv_viz=csv_viz.main_window:run_app',  # Now pointing to main_window.py
+            'csv_viz=csv_viz.main_window:run_app',
         ],
     },
 )
-
